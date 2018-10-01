cb40f7bafd165d369ed98d8fe28de51b4b1e4563
Irvel <irvel.nduva@gmail.com>
2018-10-01 18:00:29 -0500

    Add book covers gridview

--- app/src/main/java/itesm/mx/apislecturaapp/behavior/MainActivity.java
+++ app/src/main/java/itesm/mx/apislecturaapp/behavior/MainActivity.java
@@ -7,11 +7,16 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import itesm.mx.apislecturaapp.R;
+import itesm.mx.apislecturaapp.fragments.BooksIndexFragment;
 import itesm.mx.apislecturaapp.fragments.DrawerFragment;
+import itesm.mx.apislecturaapp.fragments.NewBookFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,27 +31,26 @@ public class MainActivity extends AppCompatActivity {
             CharSequence text;
             Toast toast;
             int duration = Toast.LENGTH_SHORT;
+            FragmentTransaction ft = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.your_placeholder, new BooksIndexFragment());
+                    ft.commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.your_placeholder, new NewBookFragment());
+                    ft.commit();
+                    return true;
+                case R.id.navigation_notifications:
                     // Begin the transaction
-                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft = getSupportFragmentManager().beginTransaction();
                     // Replace the contents of the container with the new fragment
                     ft.replace(R.id.your_placeholder, new DrawerFragment());
                     // or ft.add(R.id.your_placeholder, new FooFragment());
                     // Complete the changes added above
                     ft.commit();
-                    return true;
-                case R.id.navigation_dashboard:
-                    text = "DASHBOARD!";
-
-                    toast = Toast.makeText(context, text, duration);
-                    toast.show();
-                    return true;
-                case R.id.navigation_notifications:
-                    text = "NOTIFICATIONS!";
-
-                    toast = Toast.makeText(context, text, duration);
-                    toast.show();
                     return true;
             }
             return false;
@@ -68,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
     }
 
 }
--- app/src/main/java/itesm/mx/apislecturaapp/fragments/BooksIndexFragment.java
+++ app/src/main/java/itesm/mx/apislecturaapp/fragments/BooksIndexFragment.java
@@ -1,18 +1,37 @@
 package itesm.mx.apislecturaapp.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import itesm.mx.apislecturaapp.R;
+import itesm.mx.apislecturaapp.behavior.BookCoverAdapter;
+import itesm.mx.apislecturaapp.behavior.MainActivity;
 
 public class BooksIndexFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        GridView gridview = getView().findViewById(R.id.books_index_gridview);
+        gridview.setAdapter(new BookCoverAdapter(getActivity()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.drawer_fragment, container, false);
+        return inflater.inflate(R.layout.books_index_fragment, container, false);
     }
 }
